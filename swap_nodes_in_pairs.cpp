bf74faf31ed5748c2dade1266419a2b2f8f5f460
zhaoliang <yourzhaozhao@gmail.com>
2016-04-03 20:56:34 +0800

    swap nodes in pairs

---
+++ swap_nodes_in_pairs.cpp
@@ -0,0 +1,40 @@
+#include "catch.hpp"
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) { }
+};
+
+ListNode *swapPairs(ListNode *head) {
+    ListNode sentinel(0);
+    sentinel.next = head;
+    ListNode *cur = &sentinel;
+    while (cur->next && cur->next->next) {
+        ListNode *next = cur->next;
+        ListNode *second = next->next;
+        next->next = second->next;
+        second->next = next;
+        cur->next = second;
+        cur = next;
+    }
+    return sentinel.next;
+}
+
+TEST_CASE("swap nodes in pairs") {
+    ListNode head(1);
+    ListNode next(2);
+    head.next = &next;
+    ListNode second(3);
+    next.next = &second;
+    ListNode third(4);
+    second.next = &third;
+
+    auto swaped = swapPairs(&head);
+
+    REQUIRE(swaped->val == 2);
+    REQUIRE(swaped->next->val == 1);
+    REQUIRE(swaped->next->next->val == 4);
+    REQUIRE(swaped->next->next->next->val == 3);
+}
